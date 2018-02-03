eb7795bcf00248a05558aec9bb5f48d6e637def8
Brad Swan <bradj.swan@gmail.com>
2018-02-03 13:18:37 +1000

    added basic operator functions

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+/* OPERATORS */
+
+function operate(operator, a, b) {
+	
+	switch (true) {
+		case (operator === "+"):
+			return add(a, b);
+			break;
+		case (operator === "-"):
+			return subtract(a, b);
+			break;
+		case (operator === "*"):
+			return multiply(a, b);
+			break;
+		case (operator === "/"):
+			return divide(a, b);
+			break;
+	}
+}
+
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
